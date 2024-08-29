a74c4ae97eba7ec8d0dbfefa3739638e37c6aa3f
david isaac <davidisaacsurvive@gmail.com>
2024-08-29 17:14:57 +0100

    build file changes

--- build_files.sh
+++ build_files.sh
@@ -1,15 +1,3 @@
-pip install -r requirements.txt
+yum install wkhtmltopdf -y
+pip install -r requirements.txt 
 python3.9 manage.py collectstatic
-# !/bin/bash
-
-# # Ensure Python and pip are installed
-# if ! command -v pip &> /dev/null; then
-#     echo "pip not found. Installing Python and pip..."
-#     apt-get update && apt-get install -y python3-pip
-# fi
-
-# # Install requirements
-# pip install -r requirements.txt
-
-# # Collect static files
-# python3.9 manage.py collectstatic --noinput
